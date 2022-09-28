a45d07829db65bfb590663ad296ca196ad8a7a89
HarprabhGill <harprabh.gill@ucalgary.ca>
2022-09-28 00:44:34 -0600

    Added about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
                     <div className='sm:text-right pb-8 pl-4'>
                         <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
                     </div>
-                    <div></div>
-                    <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                        <div className='text-4xl font-bold'>
-                            <p>Hi. I'm Harprabh, nice to meet you. Please take a look around.</p>
-                        </div>
-                        <div>
-                            <p>I am passionate</p>
-                        </div>
+                <div></div>
+                </div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>Hi. I'm Harprabh, nice to meet you. Please take a look around.</p>
+                    </div>
+                    <div>
+                        <p>I am a passionate software developer</p>
                     </div>
                 </div>
             </div>
