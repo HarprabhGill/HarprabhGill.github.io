4a24840694e5e6516369b8c85b05be4f5d1087ad
HarprabhGill <harprabh.gill@ucalgary.ca>
2022-11-20 22:33:01 -0700

    added to virtual therapy

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -112,7 +112,7 @@ const Work = () => {
                 Virtual Therapy
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://github.com/HarprabhGill/VirtualTherapy'>
+                <a href='https://glitch.com/edit/#!/vr-therapist-cpsc581'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
