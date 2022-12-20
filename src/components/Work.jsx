f651ac808f96d48ef509b2fc4d90aef78acc4b8c
HarprabhGill <harprabh.gill@ucalgary.ca>
2022-12-20 12:09:00 -0700

    changed posture code link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -140,7 +140,7 @@ const Work = () => {
                 Posture Alert
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://glitch.com/edit/#!/vr-therapist-cpsc581'>
+                <a href='https://github.com/HarprabhGill/PostureAlert'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
